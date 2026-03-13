e2e4f999312ba8de739ddd180d1ae1e18f2dc412
lukehoop <166653189+lukehoop@users.noreply.github.com>
2026-03-13 12:52:08 -0600

    I miss SQL

--- frontend/src/components/BowlingHeading.tsx
+++ frontend/src/components/BowlingHeading.tsx
@@ -4,7 +4,7 @@ const BowlingHeading: React.FC = () => {
     return (
         <header style={{ textAlign: 'center', margin: '20px 0' }}>
             <h1>Bowling League Directory</h1>
-            <p>Welcome to the Bowling League Database! Here you'll find information about all our bowlers from the Marlins and Sharks teams.</p>
+            <p>Welcome to the database. I would give anything for this to have been just a SQL project.</p>
         </header>
     );
 };
